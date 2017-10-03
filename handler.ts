1cdf170253dc7ec4acb2e2bfe2a404e6aa4acab1
tchampion <tchampion@xebia.fr>
2017-10-03 14:20:02 +0200

    Add CORS header for findAllRecipes and return one recipe for findOneRecipes method (no array) + return 404 if not found

--- handler.ts
+++ handler.ts
@@ -3,6 +3,10 @@ import recipes from './recipes';
 const findAllRecipes = (event, context, cb) => {
     const response = {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+        },
         body: JSON.stringify(recipes)
     };
 
@@ -10,18 +14,27 @@ const findAllRecipes = (event, context, cb) => {
 };
 
 const findOneRecipes = (event, context, cb) => {
-    console.log('context :', context);
-    console.log('event :', event);
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-        },
-        body: JSON.stringify(recipes.filter(recipe => recipe.uid === event.pathParameters.uid))
-    };
+    const filtredRecipes = recipes.filter(recipe => recipe.uid === event.pathParameters.uid)
 
-    cb(null, response);
+    if (filtredRecipes.length === 0) {
+        const response = {
+            statusCode: 404,
+            body: 'Not Found'
+        };
+
+        cb(null, response);
+    } else {
+        const response = {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+                "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+            },
+            body: JSON.stringify(filtredRecipes[0])
+        };
+
+        cb(null, response);
+    }
 };
 
 export {findAllRecipes, findOneRecipes};
