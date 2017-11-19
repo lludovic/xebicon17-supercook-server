f0c83bbadf370de192bcecb792fc25cdb8dec786
tchampion <tchampion@xebia.fr>
2017-11-19 14:19:43 +0100

    Make ingredient to be object

--- recipes.ts
+++ recipes.ts
@@ -5,12 +5,18 @@ const recipes = [
         type: 'Végétarien',
         cuisson: 30,
         ingredients: [
-            "30 g fécule de pomme de terre", "30 g farine", "90 g sucre en poudre",
-            "2 oeufs Pour la crème au beurre", "1 oeuf entier", "50 g sucre", " 150 g beurre frais"],
+            {label: "30 g fécule de pomme de terre"},
+            {label: "30 g farine"},
+            {label: "90 g sucre en poudre"},
+            {label: "2 oeufs Pour la crème au beurre"},
+            {label: "1 oeuf entier"},
+            {label: "50 g sucre"},
+            {label: "150 g beurre frais"}
+        ],
         histoire: `À l'origine, il s'agissait d'une bûche qui devait brûler du 24 décembre jusqu'au Nouvel An. Allumée dans la pièce à vivre, elle apportait chaleur et lumière à la veillée. Celle-ci était souvent décorée de rubans. Si le feu faisait beaucoup d'étincelles, on disait que la moisson de l'été suivant serait bonne. Si la lumière faisait projeter des silhouettes sur le mur, on disait qu'un membre de la famille allait mourir au cours de l'année. Les cendres de la bûche étaient précieusement conservées contre les orages, pour guérir certaines maladies et pour fertiliser les terres. C'est pour continuer à célébrer ce rituel, que la buche de Noel a vu le jour sous la forme d'un délicieux dessert qui aurait été inventé par un pâtissier en 1945.
 C'est la parfaite recette de Noel à mettre sur toutes les tables !`,
-        img_small:'https://images.marmitoncdn.org/recipephotos/multiphoto/88/8800b0d9-2df6-4572-affb-d7a4c06d8331_tn-80x80.jpg',
-        img_big:'https://image.afcdn.com/recipe/20121214/39297_w420h344c1cx192cy256.jpg'
+        img_small: 'https://images.marmitoncdn.org/recipephotos/multiphoto/88/8800b0d9-2df6-4572-affb-d7a4c06d8331_tn-80x80.jpg',
+        img_big: 'https://image.afcdn.com/recipe/20121214/39297_w420h344c1cx192cy256.jpg'
     },
     {
         uid: 'DFG024543TGJVZ2139RGKG',
@@ -18,12 +24,20 @@ C'est la parfaite recette de Noel à mettre sur toutes les tables !`,
         type: 'Végétarien',
         cuisson: 20,
         ingredients: [
-            "250 g de farine", "4 œufs", "50 cl de lait", "50 g de beurre fondu", "85 g de sucre en poudre",
-            "10 cl de sirop de sucre de canne", "5 sachets de sucre vanillé", "(Bourbon de préférence) 15 cl de Cognac",
-            "1 pincée de sel", "250 g de farine"],
+            {label: "250 g de farine"},
+            {label: "4 œufs"},
+            {label: "50 cl de lait"},
+            {label: "50 g de beurre fondu"},
+            {label: "85 g de sucre en poudre"},
+            {label: "10 cl de sirop de sucre de canne"},
+            {label: "5 sachets de sucre vanillé"},
+            {label: "(Bourbon de préférence) 15 cl de Cognac"},
+            {label: "1 pincée de sel"},
+            {label: "250 g de farine"}
+        ],
         histoire: `Spécialité culinaire bretonne la crèpe se constitue d'une fine couche de pâte durcie dans la poêle que l'on déguste pour le repas ou à l'heure du goûter. Sa pâte, à base de farine de blé ou de sarrasin, de lait et d'oeuf est cuite sur les deux côtés puis roulée ou pliée en 4 selon les envies. La crêpe Suzette est une recette incontournable française imaginée par Auguste Escoffier. En Bretagne, la crêpe est le plus souvent connue sous le nom de galette. Sa pâte est réalisée avec de la farine de sarrasin et la crêpe se garnit de fromage, jambon, sucre ou pâte à tartiner...`,
-        img_small:'https://images.marmitoncdn.org/recipephotos/multiphoto/83/83f69e59-8663-4d27-bb84-63d8a43066ab_tn-80x80.jpg',
-        img_big:'https://image.afcdn.com/recipe/20170404/49483_w420h344c1cx3024cy2016.jpg'
+        img_small: 'https://images.marmitoncdn.org/recipephotos/multiphoto/83/83f69e59-8663-4d27-bb84-63d8a43066ab_tn-80x80.jpg',
+        img_big: 'https://image.afcdn.com/recipe/20170404/49483_w420h344c1cx3024cy2016.jpg'
     }
 ];
 
